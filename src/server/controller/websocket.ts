77e3b3a04835cddc4d82fb70a30a5e318f7a9b4a
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2024-09-12 13:10:08 +0200

    Add websocket compression.

--- src/server/controller/websocket.ts
+++ src/server/controller/websocket.ts
@@ -1,12 +1,49 @@
 import { VatsimDataStorage } from "@/types/data/vatsim";
 import { createClient } from "redis";
 import { vatsimDataStorage } from "@/server/storage/vatsim";
+import { WebSocketServer } from "ws";
+import { createGzip } from "zlib";
 
 const redisSub = await createClient()
     .on('error', err => console.log('Redis Client Error', err))
     .connect()
+
+const wss = new WebSocketServer({
+    port: 8080,
+    perMessageDeflate: false
+})
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error)
+    ws.on('message', async msg => {
+        console.log(msg)
+    })
+})
+
 redisSub.subscribe('vatsim_storage', (data) => {
     receiveVatsimStorage(JSON.parse(data))
+
+    const gzip = createGzip()
+    gzip.write(JSON.stringify({
+        event: 'set_vatsim',
+        data: vatsimDataStorage.position,
+    }))
+    gzip.end()
+
+    const chunks: Buffer[] = []
+    gzip.on('data', chunk => {
+        chunks.push(chunk)
+    })
+
+    gzip.on('end', () => {
+        const compressedData = Buffer.concat(chunks)
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(compressedData)
+            }
+        })
+    })
 })
 
 function receiveVatsimStorage(data: VatsimDataStorage) {
